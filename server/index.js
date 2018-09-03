b1d84d30eaa9bd7703ce34d1bbf94786e4f39118
Sergey Ch <elov4anin@gmail.com>
2018-09-03 22:59:36 +0500

    add nodemon

--- server/index.js
+++ server/index.js
@@ -1,11 +1,3 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'WORKING'
-    })
-})
-
-app.listen(5000, () => console.log('server started'));
+const app = require('./app')
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on ${port}`));
