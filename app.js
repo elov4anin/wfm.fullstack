da0cb11f7a9048895e0cd02f9dbf23ef88003d44
Sergey Ch <elov4anin@gmail.com>
2018-10-17 21:16:31 +0500

    init heroku

--- app.js
+++ app.js
@@ -51,12 +51,12 @@ app.use('/api/order', orderRoutes);
 app.use('/api/position', positionRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/dist/client'));
+    app.use(express.static('client/dist/client'));
 
     app.get('*', (req, res) => {
         res.sendFile(
             path.resolve(
-                __dirname, '../client', 'dist', 'client', 'index.html'
+                __dirname, 'client', 'dist', 'client', 'index.html'
             )
         )
     })
--- server/config/keys.dev.js
+++
@@ -1,4 +0,0 @@
-module.exports ={
-    mongoURI: 'mongodb://elov4anin:vfcnth12@ds011462.mlab.com:11462/fullstack',
-    jwt: 'dev-jwt'
-};
